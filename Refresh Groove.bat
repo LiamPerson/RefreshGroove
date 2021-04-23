d674858b5d81caa6359a0f27c81585186b55ba00
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2021-04-23 20:48:03 +1000

    Delete Refresh Groove.bat

--- Refresh Groove.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-taskkill /im Music.UI.exe /T /F
-PING localhost -n 2 >NUL
-del /s /q C:\Users\Yelo\AppData\Local\Packages\Microsoft.ZuneMusic_8wekyb3d8bbwe\LocalState\Database\anonymous\*
